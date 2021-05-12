78b52045c864cd635ad133e29d2b42581e0428d5
SilWolf <silwolf1121@gmail.com>
2021-05-13 01:39:09 +0800

    feat: auto refresh first draft

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         bahaGuildV2Toolkit
+// @name         Bahamut Guild V2 Toolkits
 // @namespace    https://silwolf.io/
 // @version      0.1
 // @description  巴哈公會2.0的插件
 // @author       銀狼(silwolf167)
-// @include      /guild.gamer.com.tw/guild2.0.php
+// @include      /guild.gamer.com.tw/guild.php
 // @include      /guild.gamer.com.tw/post_detail.php
 // @grant        none
 // ==/UserScript==
@@ -19,35 +19,139 @@
 			const style = document.createElement('style');
 			style.type = 'text/css';
 			style.innerHTML = `
-.text_content-hide {
-  display: block !important;
-}
+        .text_content-hide {
+          display: block !important;
+        }
 
-.more-text {
-  display: none;
-}
+        .more-text {
+          display: none;
+        }
 
-div[data-google-query-id] { display: none; }
+        div[data-google-query-id] {
+          display: none;
+        }
 
-.webview_commendlist { display: flex; flex-direction: column-reverse; }
-.webview_commendlist > div { display: flex; flex-direction: column-reverse; }
-`;
+        .webview_commendlist {
+          display: flex; flex-direction: column-reverse;
+        }
+        .webview_commendlist > div {
+          display: flex; flex-direction: column-reverse;
+        }
+      `;
 
-      if (location && location.href.includes('post_detail.php')) {
-        style.innerHTML += `
-.webview_commendlist .c-reply__editor { position: sticky;
-  top: 50px;
-  margin-left: -20px;
-  margin-right: -20px;
-  padding-left: 20px;
-  padding-right: 20px;
-  background-color: #fff;
-  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
-}
-`
+			if (location && location.href.includes('post_detail.php')) {
+				style.innerHTML += `
+          .webview_commendlist .c-reply__editor {
+            position: sticky;
+            top: 80px;
+            margin-left: -20px;
+            margin-right: -20px;
+            padding-left: 20px;
+            padding-right: 20px;
+            background-color: #bfbfbf;
+            box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+          }
 
-      }
+          .reply-input textarea {
+            min-height: 66px;
+          }
+        `;
+			}
 			head.appendChild(style);
 		}
+
+		// const editor = document.getElementsByClassName('c-reply__editor');
+		// console.log(
+		// 	document.getElementsByClassName('c-reply__editor'),
+		// 	document.getElementsByClassName('c-reply__editor')[0]
+		// );
+		// if (editor) {
+		// 	const newButton = document.createElement('button');
+		// 	newButton.innerHTML('重新下載');
+
+		// 	editor.appendChild(newButton);
+		// }
+
+		const gsn = guild.gsn;
+		const sn = parseInt($('.inboxfeed').first().data('post-sn'));
+
+		let comments = [];
+		const apiUrl = `https://api.gamer.com.tw/guild/v1/comment_list.php?gsn=${gsn}&messageId=${sn}&all`;
+
+		const appendNewComments = (newComments) => {
+			let commentListHtml = '';
+			for (const nC of newComments) {
+				nC.text = GuildTextUtil.mentionTagToMarkdown(
+					gsn,
+					nC.text,
+					nC.tags,
+					nC.mentions
+				);
+				commentListHtml += nunjucks.render('comment.njk.html', {
+					post: {
+						id: sn,
+						commentCount: 0,
+						to: { gsn: gsn },
+					},
+					comment: nC,
+					marked: GuildTextUtil.markedInstance,
+					youtubeParameterMatcher: GuildTextUtil.youtubeParameterMatcher,
+					user: guildPost.loginUser,
+				});
+			}
+
+			if (commentListHtml) {
+				let $post = Guild.getPost(sn, false, '');
+				let $commentContainer = $post.find(
+					'.webview_commendlist > div:first-child'
+				);
+				$commentContainer.append(commentListHtml);
+
+				comments = [...comments, ...newComments];
+			}
+		};
+
+		fetch(apiUrl, {
+			credentials: 'include',
+		})
+			.then((res) => res.json())
+			.then((response) => {
+				comments = response.data.comments;
+
+				setInterval(() => {
+					fetch(apiUrl, {
+						credentials: 'include',
+					})
+						.then((res) => res.json())
+						.then((newResponse) => {
+							const newComments = newResponse.data.comments;
+							appendNewComments(
+								newComments.filter(
+									(nC) => comments.findIndex((c) => c.id === nC.id) === -1
+								)
+							);
+						});
+				}, 5000);
+			});
+
+		setInterval(() => {
+			// const manageData = {
+			// 	admin: false,
+			// 	pinPost: [],
+			// 	deletePost: { 3014: true },
+			// };
+			// const guildPost = new GuildPost('.inboxfeed', manageData);
+			// guildPost.fetchPostDetail(4158, 15080367);
+			// GuildComment.fetchCommentList(15080367, '', false);
+			// fetch(
+			// 	`https://api.gamer.com.tw/guild/v1/comment_list.php?gsn=4158&messageId=15080367`,
+			// 	{
+			// 		credentials: 'include',
+			// 	}
+			// );
+			// 1. 將已有comments變成一個array
+			// 2. 在新的fetch response中找到新的message, 然後append到comments中
+			// 3. 對每個新的comment, append HTML
+		}, 5000);
 	});
 })();
