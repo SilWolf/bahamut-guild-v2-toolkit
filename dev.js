b2a5362160f22f3ec3a78e523773618559d29a54
Dicky Chan <hinyiuchan@hk01.com>
2021-05-20 12:45:28 +0800

    feat: plugin config form and getConfig from it

---
+++ dev.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Bahamut Guild V2 Toolkits
+// @namespace    https://silwolf.io/
+// @version      0.1
+// @description  巴哈公會2.0的插件
+// @author       銀狼(silwolf167)
+// @include      /guild.gamer.com.tw/guild.php
+// @include      /guild.gamer.com.tw/post_detail.php
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      file:///Users/user/Desktop/bahamut-guild-v2-toolkit/index.js
+// ==/UserScript==
+
+;(function () {
+	'use strict'
+
+	// Your code here...
+})()
--- index.js
+++ index.js
@@ -9,16 +9,77 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-	'use strict';
+;(function () {
+	'use strict'
 
 	// Your code here...
 	jQuery(document).ready(() => {
-		const head = document.getElementsByTagName('head')[0];
+		const head = document.getElementsByTagName('head')[0]
 		if (head) {
-			const style = document.createElement('style');
-			style.type = 'text/css';
+			const style = document.createElement('style')
+			style.type = 'text/css'
 			style.innerHTML = `
+				/* The switch - the box around the slider */
+				.switch {
+					position: relative;
+					display: inline-block;
+					width: 30px;
+					height: 17px;
+				}
+
+				/* Hide default HTML checkbox */
+				.switch input {
+					opacity: 0;
+					width: 0;
+					height: 0;
+				}
+
+				/* The slider */
+				.slider {
+					position: absolute;
+					cursor: pointer;
+					top: 0;
+					left: 0;
+					right: 0;
+					bottom: 0;
+					background-color: #ccc;
+					-webkit-transition: .4s;
+					transition: .4s;
+				}
+
+				.slider:before {
+					position: absolute;
+					content: "";
+					height: 13px;
+					width: 13px;
+					left: 2px;
+					bottom: 2px;
+					background-color: white;
+				}
+
+				input:checked + .slider {
+					background-color: #2196F3;
+				}
+
+				input:focus + .slider {
+					box-shadow: 0 0 1px #2196F3;
+				}
+
+				input:checked + .slider:before {
+					-webkit-transform: translateX(13px);
+					-ms-transform: translateX(13px);
+					transform: translateX(13px);
+				}
+
+				/* Rounded sliders */
+				.slider.round {
+					border-radius: 17px;
+				}
+
+				.slider.round:before {
+					border-radius: 50%;
+				}
+
         .text_content-hide {
           display: block !important;
         }
@@ -37,7 +98,56 @@
         .webview_commendlist > div {
           display: flex; flex-direction: column-reverse;
         }
-      `;
+        .webview_commendlist > div.c-reply__editor {
+          flex-direction: column;
+        }
+
+				.webview_commendlist > div.c-reply__editor .plugin-config-wrapper {
+					text-align: right;
+					padding: 13px 0 5px;
+					font-size: 12px;
+				}
+
+				.plugin-config-form {
+					background: #ffffff;
+					padding: 8px;
+					border-radius: 4px;
+
+					display: none;
+				}
+
+				.plugin-config-form.active {
+					display: block;
+				}
+
+				.plugin-config-form.plugin-config-form.plugin-config-form input {
+					border: 1px solid #999;
+				}
+
+				.plugin-config-form.plugin-config-form.plugin-config-form button {
+					-webkit-border-radius: 5px;
+					-moz-border-radius: 5px;
+					border-radius: 5px;
+					background-color: #eee;
+					padding: 3px;
+					border: 1px solid #333;
+					color: black;
+					text-decoration: none;
+				}
+
+				.plugin-config-form .form-message {
+					text-align: center;
+					color: #4a934a;
+					font-size: 12px;
+					min-height: 24px;
+					line-height: 16px;
+					padding: 4px;
+				}
+
+				.plugin-config-form .form-footer {
+					text-align: center;
+				}
+      `
 
 			if (location && location.href.includes('post_detail.php')) {
 				style.innerHTML += `
@@ -48,16 +158,16 @@
             margin-right: -20px;
             padding-left: 20px;
             padding-right: 20px;
-            background-color: #bfbfbf;
+            background-color: #cfcfcf;
             box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
           }
 
           .reply-input textarea {
             min-height: 66px;
           }
-        `;
+        `
 			}
-			head.appendChild(style);
+			head.appendChild(style)
 		}
 
 		// const editor = document.getElementsByClassName('c-reply__editor');
@@ -72,23 +182,23 @@
 		// 	editor.appendChild(newButton);
 		// }
 
-		let hasTakenOver = false;
+		let hasTakenOver = false
 
-		const gsn = guild.gsn;
-		const sn = parseInt($('.inboxfeed').first().data('post-sn'));
+		const gsn = guild.gsn
+		const sn = parseInt($('.inboxfeed').first().data('post-sn'))
 
-		let comments = [];
-		const apiUrl = `https://api.gamer.com.tw/guild/v1/comment_list.php?gsn=${gsn}&messageId=${sn}`;
+		let comments = []
+		const apiUrl = `https://api.gamer.com.tw/guild/v1/comment_list.php?gsn=${gsn}&messageId=${sn}`
 
 		const appendNewComments = (newComments) => {
-			let commentListHtml = '';
+			let commentListHtml = ''
 			for (const nC of newComments) {
 				nC.text = GuildTextUtil.mentionTagToMarkdown(
 					gsn,
 					nC.text,
 					nC.tags,
 					nC.mentions
-				);
+				)
 				commentListHtml += nunjucks.render('comment.njk.html', {
 					post: {
 						id: sn,
@@ -99,29 +209,29 @@
 					marked: GuildTextUtil.markedInstance,
 					youtubeParameterMatcher: GuildTextUtil.youtubeParameterMatcher,
 					user: guildPost.loginUser,
-				});
+				})
 			}
 
 			if (commentListHtml) {
-				let $post = Guild.getPost(sn, false, '');
+				let $post = Guild.getPost(sn, false, '')
 				let $commentContainer = $post.find(
 					'.webview_commendlist > div:first-child'
-				);
-				$commentContainer.append(commentListHtml);
+				)
+				$commentContainer.append(commentListHtml)
 
-				comments = [...comments, ...newComments];
+				comments = [...comments, ...newComments]
 			}
-		};
+		}
 
 		fetch(`${apiUrl}&all`, {
 			credentials: 'include',
 		})
 			.then((res) => res.json())
 			.then((response) => {
-				comments = response.data.comments;
+				comments = response.data.comments
 
 				if (!hasTakenOver) {
-					const oldGuildCommentReplyEnterKey = GuildComment.replyEnterKey;
+					const oldGuildCommentReplyEnterKey = GuildComment.replyEnterKey
 					// GuildComment.replyEnterKey = (event, element) => {
 					// 	let $element = $(element);
 					// 	let gsn = parseInt($element.data('gsn'), 10);
@@ -142,21 +252,120 @@
 					// 	}
 					// };
 
-					const editor = document.getElementsByClassName('reply-input')[0];
-					console.log(editor);
+					// 將各個關鍵元件放進變量中
+					const editorContainer =
+						document.getElementsByClassName('c-reply__editor')[0]
+					const editor =
+						editorContainer.getElementsByClassName('reply-input')[0]
+					const editorTextarea = editor.getElementsByTagName('textarea')[0]
 
-					const textarea = editor.getElementsByTagName('textarea')[0];
+					// pluginConfigA - 建立可開關插件設定板面的連結
+					const pluginConfigAWrapper = document.createElement('div')
+					pluginConfigAWrapper.classList.add('plugin-config-wrapper')
+					editorContainer.appendChild(pluginConfigAWrapper)
 
-					console.log(textarea);
+					const pluginConfigA = document.createElement('a')
+					pluginConfigA.innerHTML = '插件設定'
+					pluginConfigA.setAttribute('href', '#')
+					pluginConfigAWrapper.appendChild(pluginConfigA)
+
+					// pluginConfigForm - 插件設定板面
+					const pluginConfigForm = document.createElement('form')
+					pluginConfigForm.classList.add('plugin-config-form')
+					pluginConfigForm.innerHTML = `
+						<div class="form-group">
+							<label class="switch">
+								<input type="checkbox" data-field="isEnabledAutoRefresh" data-type="boolean">
+								<span class="slider"></span>
+							</label>
+							<span>自動更新</span>
+							<input type="number" min="1" data-field="autoRefreshInterval" data-type="number" style="width: 40px;" />
+							<span>秒</span>
+						</div>
+
+						<div class="form-message"></div>
+
+						<div class="form-footer">
+							<button type="button" class="plugin-config-button-set-as-default">設為預設值</button>
+							<button type="button" class="plugin-config-button-apply">套用</button>
+						</div>
+					`
+					editorContainer.appendChild(pluginConfigForm)
+
+					// PluginConfigFormMessage - 插件設定板面訊息
+					const PluginConfigFormMessage =
+						pluginConfigForm.getElementsByClassName('form-message')[0]
+					let PluginConfigFormMessageTimeout
+					const showPluginConfigMessage = (text, duration = 1500) => {
+						PluginConfigFormMessage.innerHTML = text
+
+						if (PluginConfigFormMessageTimeout) {
+							clearTimeout(PluginConfigFormMessageTimeout)
+						}
+						PluginConfigFormMessageTimeout = setTimeout(() => {
+							PluginConfigFormMessage.innerHTML = ''
+							PluginConfigFormMessageTimeout = undefined
+						}, duration)
+					}
+
+					// getConfig - 從插件設定板面中讀取設定
+					const getConfig = () => {
+						const els = pluginConfigForm.querySelectorAll('[data-field]')
+						const result = {}
+
+						for (const el of els) {
+							const field = el.getAttribute('data-field')
+							const type = el.getAttribute('data-type')
+							const value = el.value
+
+							switch (type) {
+								case 'boolean':
+									result[field] = el.checked
+									break
+								case 'number':
+									result[field] = parseInt(value)
+									break
+								default:
+									result[field] = value
+							}
+						}
+
+						return result
+					}
+
+					// pluginConfigApplyButton - 插件設定板面的「套用」按鈕
+					const pluginConfigApplyButton =
+						pluginConfigForm.getElementsByClassName(
+							'plugin-config-button-apply'
+						)[0]
+					const handleClickPluginConfigApply = () => {
+						getConfig()
+						showPluginConfigMessage('已套用設定')
+					}
+					pluginConfigApplyButton.addEventListener(
+						'click',
+						handleClickPluginConfigApply
+					)
+
+					// togglePluginConfigForm - 開關插件設定板面
+					const togglePluginConfigForm = (newState) => {
+						pluginConfigForm.classList.toggle('active', newState)
+					}
+
+					const handleClickPluginConfigA = (event) => {
+						event.preventDefault()
+						togglePluginConfigForm()
+					}
+					pluginConfigA.addEventListener('click', handleClickPluginConfigA)
 
 					const onKeyDownFn = () => {
-						console.log('123');
-					};
-					textarea.addEventListener('keydown', onKeyDownFn);
+						console.log('123')
+					}
+					editorTextarea.addEventListener('keydown', onKeyDownFn)
 
 					// editor.innerHTML = '';
 
-					hasTakenOver = true;
+					hasTakenOver = true
 				}
 
 				setInterval(() => {
@@ -165,14 +374,14 @@
 					})
 						.then((res) => res.json())
 						.then((newResponse) => {
-							const newComments = newResponse.data.comments;
+							const newComments = newResponse.data.comments
 							appendNewComments(
 								newComments.filter(
 									(nC) => comments.findIndex((c) => c.id === nC.id) === -1
 								)
-							);
-						});
-				}, 5000000);
-			});
-	});
-})();
+							)
+						})
+				}, 5000000)
+			})
+	})
+})()
