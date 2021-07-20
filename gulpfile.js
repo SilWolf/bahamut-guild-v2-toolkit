4e16a404f51d2ed2262b800003cec0ad0a250099
SilWolf <silwolf1121@gmail.com>
2021-07-20 13:42:46 +0800

    feat: use typescript with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 const gulp = require('gulp')
 
 // Gulp 插件
-const gulpConcat = require('gulp-concat')
+const concat = require('gulp-concat')
+const ts = require('gulp-typescript')
+const clean = require('gulp-clean')
+const merge = require('merge2')
 
-gulp.task('concat', function () {
+gulp.task('concat', () => {
 	return gulp
-		.src('src/**/*.js')
-		.pipe(gulpConcat('index.js'))
+		.src(['src/core/metadata.js', 'src/core/index.js', 'src/**/*.js'])
+		.pipe(concat('index.js'))
 		.pipe(gulp.dest('dist'))
 })
+
+gulp.task('clean', () => {
+	return gulp.src('dist', { read: false, allowEmpty: true }).pipe(clean())
+})
+
+gulp.task(
+	'build',
+	gulp.series('clean', () => {
+		var tsResult = gulp.src('src/**/*.ts').pipe(
+			ts({
+				declaration: true,
+			})
+		)
+
+		return merge([
+			tsResult.dts.pipe(gulp.dest('dist/definitions')),
+			tsResult.js
+				.pipe(gulp.dest('dist/js'))
+				.pipe(concat('index.js'))
+				.pipe(gulp.dest('dist')),
+		])
+	})
+)
