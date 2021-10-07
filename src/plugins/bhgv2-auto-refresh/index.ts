0e48ea0e7aed1afd88bf29bc0220bc0852f67cb6
Dicky Chan <hinyiuchan@hk01.com>
2021-10-07 18:35:56 +0800

    feat: slow mode refresh (incomplete)

--- src/plugins/bhgv2-auto-refresh/index.ts
+++ src/plugins/bhgv2-auto-refresh/index.ts
@@ -35,6 +35,14 @@ const BHGV2_AutoRefresh: TPluginConstructor = (core) => {
 			defaultValue: 30,
 		},
 		{
+			key: `${_plugin.prefix}:autoSlowDown`,
+			suffixLabel:
+				'如果串最後更新時間超過15分鐘，自動進入低頻率更新模式(建議開啟)',
+			dataType: 'boolean',
+			inputType: 'switch',
+			defaultValue: true,
+		},
+		{
 			key: `${_plugin.prefix}:notification`,
 			suffixLabel: '自動更新時發送桌面通知',
 			dataType: 'boolean',
@@ -52,6 +60,7 @@ const BHGV2_AutoRefresh: TPluginConstructor = (core) => {
 
 	_plugin.configLayout = [
 		[`${_plugin.prefix}:isEnable`, `${_plugin.prefix}:interval`],
+		[`${_plugin.prefix}:autoSlowDown`],
 		[`${_plugin.prefix}:notification`, `${_plugin.prefix}:notificationSound`],
 	]
 
@@ -85,10 +94,18 @@ const BHGV2_AutoRefresh: TPluginConstructor = (core) => {
 
 				const _config = core.getConfig()
 
-				let _interval = parseInt(
-					(newValue[`${_plugin.prefix}:interval`] as string) ||
-						(_config[`${_plugin.prefix}:interval`] as string)
-				)
+				let _interval = 30
+
+				if (_config[`${_plugin.prefix}:slowMode`] === 2) {
+					_interval = 60 * 60
+				} else if (_config[`${_plugin.prefix}:slowMode`] === 1) {
+					_interval = 5 * 60
+				} else {
+					_interval = parseInt(
+						(newValue[`${_plugin.prefix}:interval`] as string) ||
+							(_config[`${_plugin.prefix}:interval`] as string)
+					)
+				}
 
 				if (!_interval || _interval <= 0) {
 					_interval = 30
@@ -179,39 +196,63 @@ const BHGV2_AutoRefresh: TPluginConstructor = (core) => {
 			return
 		}
 
+		if (!newValue.latestComments) {
+			return
+		}
+
+		const _comment = newValue.latestComments[0]
+
+		// Check if enter Slow-down mode
+		if (_comment.payload?.ctime) {
+			const commentCTime = new Date(_comment.payload?.ctime).getTime()
+			const nowTime = Date.now()
+
+			if (nowTime - commentCTime > 12 * 60 * 60 * 1000) {
+				// over 12 hours
+				core.mutateConfig({
+					[`${_plugin.prefix}:slowMode`]: 2,
+				})
+			} else if (nowTime - commentCTime > 15 * 60 * 1000) {
+				// over 15 minutes
+				core.mutateConfig({
+					[`${_plugin.prefix}:slowMode`]: 1,
+				})
+			} else {
+				core.mutateConfig({
+					[`${_plugin.prefix}:slowMode`]: undefined,
+				})
+			}
+		}
+
 		const config = core.getConfig()
 		if (config[`${_plugin.prefix}:notification`]) {
-			if (newValue.latestComments !== undefined) {
-				const _comment = newValue.latestComments[0]
+			let text = '[如果你看到這句話，代表有東西爆炸了，請聯絡月月處理……]'
+			if (_comment.payload) {
+				const _payload = _comment.payload
+				text = `(#${_payload.position}) ${
+					_payload.name
+				}：${_payload.text.substr(0, 50)}`
+			} else if (_comment.element) {
+				const _element = _comment.element
+				const _name = _element.getAttribute('data-user')
+				const _position = _element.getAttribute('data-position')
+				const _text =
+					_comment.element.querySelector('.reply-content__cont')?.textContent ||
+					''
+				text = `(#${_position}) ${_name}：${_text.substr(0, 50)}`
+			}
 
-				let text = '[如果你看到這句話，代表有東西爆炸了，請聯絡月月處理……]'
-				if (_comment.payload) {
-					const _payload = _comment.payload
-					text = `(#${_payload.position}) ${
-						_payload.name
-					}：${_payload.text.substr(0, 50)}`
-				} else if (_comment.element) {
-					const _element = _comment.element
-					const _name = _element.getAttribute('data-user')
-					const _position = _element.getAttribute('data-position')
-					const _text =
-						_comment.element.querySelector('.reply-content__cont')
-							?.textContent || ''
-					text = `(#${_position}) ${_name}：${_text.substr(0, 50)}`
-				}
+			// 發送桌面通知
+			createNotification({
+				title: '有新的通知',
+				text: text,
+				silent: !config[`${_plugin.prefix}:notificationSound`],
+				timeout: 5000,
+			})
 
-				// 發送桌面通知
-				createNotification({
-					title: '有新的通知',
-					text: text,
-					silent: !config[`${_plugin.prefix}:notificationSound`],
-					timeout: 5000,
-				})
-
-				// 播放通知音
-				if (config[`${_plugin.prefix}:notification`]) {
-					notifyAudio.play()
-				}
+			// 播放通知音
+			if (config[`${_plugin.prefix}:notification`]) {
+				notifyAudio.play()
 			}
 		}
 	}
