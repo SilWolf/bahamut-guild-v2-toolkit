0b1717027ab4f82f48e6abbef8345542b7722b0c
SilWolf <silwolf1121@gmail.com>
2024-03-22 14:25:39 +0800

    chore: remove all files for entire new framework

--- src/helpers/string.helper.ts
+++
@@ -1,9 +0,0 @@
-const characters =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz0123456789_'
-
-export const generateRandomId = (): string => {
-  return new Array(12)
-    .fill('0')
-    .map(() => characters[Math.floor(characters.length * Math.random())])
-    .join('')
-}
--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare var GM_notification
-
-declare var jQuery: any
-declare var $: any
-declare var Bahamut: any
-declare var guild: { gsn: number }
-declare var guildPost: any
-declare var GuildTextUtil: any
-declare var Dialogify: any
